ed91a3d453d8ad1089be70f7f3437d175d5577e8
Srivarshan <srivarshan02@gmail.com>
2023-03-07 20:45:16 +0530

    Add code to automatically detect the language

--- twitter-scraper/process.py
+++ twitter-scraper/process.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import pycld2 as cld
 import re
 
 
@@ -34,11 +35,35 @@ def replace_tags(dataframe, columns):
     return dataframe
 
 
+def detect_lang(dataframe, column):
+
+    lang_list = []
+    texts = dataframe[column]
+
+    for text in texts:
+        # Remove @ mentions and urls
+        text = re.sub(r"(?:\@|https?\://)\S+", "", text)
+        _, _, _, lang = cld.detect(text,  returnVectors=True)
+        lang = [ele[2] for ele in lang]
+        # Check for english
+        if len(lang) == 1 and lang[0] != "Unknown":
+            lang_list.append(lang[0])
+        else:
+            lang_list.append("CODE-MIXED")
+
+    dataframe["Language"] = lang_list
+
+    return dataframe
+
+
 if __name__ == "__main__":
 
     df = pd.read_csv("tweet_comments.csv")
 
-    df = replace_tags(dataframe=df, columns=[
-                      "Replying to", "Tweet", "Parent Tweet"])
+    df = replace_tags(dataframe=df, 
+            columns=["Replying to", "Tweet", "Parent Tweet"])
 
+    df = detect_lang(dataframe=df, column="Tweet")
+    
     print(df)
+    df.to_csv("tweet_comments_processed.csv", index=False, encoding="utf-8")
