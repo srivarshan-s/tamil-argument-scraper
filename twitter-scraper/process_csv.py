cfdbd685ecb2b837bc1cb0010c233ce443e89a3f
Srivarshan <srivarshan02@gmail.com>
2023-03-07 09:20:48 +0530

    Add code to extract user mentions

--- twitter-scraper/process_csv.py
+++ twitter-scraper/process_csv.py
@@ -1,11 +1,23 @@
 import pandas as pd
 import numpy as np
+import re
 
-df = pd.read_csv("tweet_comments.csv")
-# print(df.head())
 
-user_tags = np.unique(df["Replying to"]) 
-# print(user_tags)
+if __name__ == "__main__":
 
-for tweet in df["Tweet"]:
-    print(tweet)
+    df = pd.read_csv("tweet_comments.csv")
+    # print(df.head())
+
+    user_tags = list(df["Replying to"]) 
+    # print(user_tags)
+
+    mention_re = "(^|[^@\w])@(\w{1,15})"
+
+    for tweet in df["Tweet"]:
+        mentions = re.findall(mention_re, tweet)
+        for _, tag in mentions:
+            tag = '@' + tag
+            user_tags.append(tag)
+
+    user_tags = np.unique(user_tags)
+    print(user_tags)
